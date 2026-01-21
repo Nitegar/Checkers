80ef429db6aed85c997f29c988cc5f0a6c739535
kingo <met2rasool@gmail.com>
2026-01-21 20:40:06 +0100

    chore: Replaced busy-wait while(true) with clean thread lifecycle management using non-daemon UI threads and Thread.join() to keep the application alive as long as GUI/TUI are running.

--- src/main/scala/de/htwg/CheckersApp.scala
+++ src/main/scala/de/htwg/CheckersApp.scala
@@ -7,6 +7,7 @@ import de.htwg.view.gui.GuiViewFx
 import de.htwg.view.tui.TuiView
 
 object CheckersApp {
+
   def main(args: Array[String]): Unit = {
     val mode = args.headOption.getOrElse("--parallel")
     val injector = Guice.createInjector(new CheckersModule(mode))
@@ -19,23 +20,27 @@ object CheckersApp {
     logicThread.setDaemon(true)
     logicThread.start()
 
+    var tuiThread: Option[Thread] = None
+    var guiThread: Option[Thread] = None
+
     // 2. Start TUI Thread
     if (mode == "--parallel" || mode == "--tui") {
       val tui = new TuiView(inputHandler)
       controller.add(tui)
-      val tuiThread = new Thread(() => tui.run())
-      tuiThread.setDaemon(true)
-      tuiThread.start()
+      val t = new Thread(() => tui.run(), "tui-thread")
+      t.start()
+      tuiThread = Some(t)
     }
 
     // 3. Start GUI (This blocks the main thread)
     if (mode == "--parallel" || mode == "--gui") {
       val gui = new GuiViewFx(inputHandler)
-      // We don't wrap this in a thread; we let it own the main thread
-      gui.run(controller)
+      val t = new Thread(() => gui.run(controller), "gui-thread")
+      t.start()
+      guiThread = Some(t)
     }
-    while (true) {
-      Thread.sleep(1000)
-    }
+
+    tuiThread.foreach(_.join())
+    guiThread.foreach(_.join())
   }
 }
